71a64227905e92371c4bfad4525185ef4163a82b
Linus Pettersson and Nicklas Ramhöj <dev+linus+nicklas@elabs.se>
2016-03-24 12:27:24 +0100

    Add more tests of Payload

--- lib/apns/payload.ex
+++ lib/apns/payload.ex
@@ -28,36 +28,6 @@ defmodule APNS.Payload do
       payload = put_in(payload[:aps][:alert], format_loc(msg.alert))
     end
 
-    to_json(payload, limit)
-  end
-
-  def to_binary(msg, payload) do
-    token_bin = msg.token |> Base.decode16!(case: :mixed)
-    frame = <<
-      1                  :: 8,
-      32                 :: 16,
-      token_bin          :: binary,
-      2                  :: 8,
-      byte_size(payload) :: 16,
-      payload            :: binary,
-      3                  :: 8,
-      4                  :: 16,
-      msg.id             :: 32,
-      4                  :: 8,
-      4                  :: 16,
-      msg.expiry         :: 32,
-      5                  :: 8,
-      1                  :: 16,
-      msg.priority       :: 8
-    >>
-    <<
-      2                 ::  8,
-      byte_size(frame)  ::  32,
-      frame             ::  binary
-    >>
-  end
-
-  defp to_json(payload, limit) do
     json = Poison.encode!(payload)
 
     length_diff = byte_size(json) - limit
@@ -75,6 +45,34 @@ defmodule APNS.Payload do
     end
   end
 
+  def to_binary(msg, payload) do
+    token_bin = msg.token |> Base.decode16!(case: :mixed)
+
+    frame = <<
+      1                  :: 8,
+      32                 :: 16,
+      token_bin          :: binary,
+      2                  :: 8,
+      byte_size(payload) :: 16,
+      payload            :: binary,
+      3                  :: 8,
+      4                  :: 16,
+      msg.id             :: 32,
+      4                  :: 8,
+      4                  :: 16,
+      msg.expiry         :: 32,
+      5                  :: 8,
+      1                  :: 16,
+      msg.priority       :: 8
+    >>
+
+    <<
+      2                 ::  8,
+      byte_size(frame)  ::  32,
+      frame             ::  binary
+    >>
+  end
+
   defp truncate(%{body: string} = alert, size) do
     %{alert | body: truncate(string, size)}
   end
@@ -89,29 +87,35 @@ defmodule APNS.Payload do
     end
   end
 
-  defp format_loc(%APNS.Message.Loc{title: title, body: body, title_loc_key: title_loc_key,
-                                    title_loc_args: title_loc_args, action_loc_key: action_loc_key,
-                                    loc_key: loc_key, loc_args: loc_args,
-                                    launch_image: launch_image}) do
+  defp format_loc(%APNS.Message.Loc{
+    title: title, body: body, title_loc_key: title_loc_key,
+    title_loc_args: title_loc_args, action_loc_key: action_loc_key,
+    loc_key: loc_key, loc_args: loc_args, launch_image: launch_image}) do
+
     # These are required parameters
     alert = %{title: title, body: body, "loc-key": loc_key, "loc-args": loc_args}
+
     # Following are optional parameters
     if title_loc_key != nil do
       alert = alert
       |> Map.put(:'title-loc-key', title_loc_key)
     end
+
     if title_loc_args != nil do
       alert = alert
       |> Map.put(:'title-loc-args', title_loc_args)
     end
+
     if action_loc_key != nil do
       alert = alert
       |> Map.put(:'action-loc-key', action_loc_key)
     end
+
     if launch_image != nil do
       alert = alert
       |> Map.put(:'launch-image', launch_image)
     end
+
     alert
   end
 end
